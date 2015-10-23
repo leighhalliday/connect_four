896b21a221d178784f4dadef927b6dedccc05c35
Leigh Halliday <leigh.halliday@thescore.com>
2015-10-23 09:27:27 -0400

    Printing the board

--- lib/connect_four/board.ex
+++ lib/connect_four/board.ex
@@ -21,4 +21,34 @@ defmodule ConnectFour.Board do
     for row <- 1..@last_row, column <- 1..@last_column, do: {row, column}
   end
 
+  def print do
+    for row <- @last_row..1, do: print_columns(row)
+  end
+
+  defp print_columns(row) do
+    for col <- 1..@last_column, do: print_space(row, col)
+    IO.write "\n"
+  end
+
+  defp print_space(row, col) do
+    agent_name(row, col)
+      |> Process.whereis
+      |> Agent.get(fn x -> x end)
+      |> convert_for_display
+      |> IO.write
+  end
+
+  defp agent_name(row, col) do
+    String.to_atom("R" <> Integer.to_string(row) <> "C" <> Integer.to_string(col))
+  end
+
+  defp convert_for_display(agent_state) do
+    case agent_state do
+      Empty -> "."
+      :red -> "R"
+      :black -> "B"
+      _ -> "?"
+    end
+  end
+
 end
